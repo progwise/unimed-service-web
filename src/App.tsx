6a476e4d237ad3d26603b1c7cf9046e788b836cf
Michael H. Vogt <michael.vogt@progwise.net>
2023-06-09 17:24:28 +0200

    added typesense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Stack } from "@mui/material";
-import "./App.css";
 import { ServiceCard } from "./ServiceCard";
+import { Stack } from '@mui/material'
+import './App.css'
 
 function App() {
   return (
     <main>
       <img src="logo_m_siegel.jpg" />
+
       <h1>Unimed Greifswald Service Web</h1>
       <Stack direction="row" flexWrap={"wrap"}>
         {[0, 1, 2, 3, 4, 5].map((item) => (
