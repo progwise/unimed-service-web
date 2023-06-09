74c57b7cdbec86142c33270a912eb64407be8b75
Michael H. Vogt <michael.vogt@progwise.net>
2023-06-09 18:15:37 +0200

    added typesense sync job

--- indexBuilder/sync.ts
+++ indexBuilder/sync.ts
@@ -1,5 +1,6 @@
 import { Client } from 'typesense'
-const typesense = new Client({
+import { FieldType } from 'typesense/lib/Typesense/Collection';
+const client = new Client({
   nodes: [
     {
       host: process.env.FGE_TYPESENSE_API_HOST ?? '',
@@ -9,3 +10,32 @@ const typesense = new Client({
   ],
   apiKey: process.env.FGE_TYPESENSE_API_KEY ?? '',
 })
+
+const upsertCollection = async (
+  name: string,
+  schemaFields: Array<{ name: string; type: FieldType; facet: boolean}>,
+  data: Array<Record<string, string>>
+) => {
+  const schema = {name, fields: schemaFields}
+  // const exists = await client.collections(name).exists()
+  // if (exists) {
+  //   await client.collections(name).delete()
+  // }
+  await client.collections().create(schema)
+
+  const upsertPromises = data.map(document => client.collections(name).documents().upsert(document))
+  await upsertPromises
+}
+
+await upsertCollection('test1', [
+  {
+    name: 'name',
+    facet: true,
+    type: 'string'
+  },],
+  [
+    {
+      name: 'test1',
+    }
+  ]
+)
