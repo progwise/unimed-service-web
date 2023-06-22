22581df2f8ab56963d6693338fc92776962180d8
Michael H. Vogt <michael.vogt@progwise.net>
2023-06-22 17:25:30 +0200

    fix

--- src/PersonCard.tsx
+++ src/PersonCard.tsx
@@ -17,7 +17,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert'
 import PersonIcon from '@mui/icons-material/Person'
 export const PersonCard = (params: { item: Record<string, string | string[]> }) => {
   console.log(params.item)
-  const schlagworte = !Array.isArray(params.item.schlagworte) ? params.item.schlagworte.split(',') : params.item.schlagworte
+  const schlagworte = !Array.isArray(params.item?.schlagworte) ? params.item.schlagworte?.split(',') : params.item.schlagworte
 
   return (
     <Card sx={{ maxWidth: 345 }}>
@@ -41,7 +41,7 @@ export const PersonCard = (params: { item: Record<string, string | string[]> }) 
       ></CardHeader>
       <CardContent>
       <Stack direction="row" spacing={1} flexWrap={'wrap'} gap={1}>
-          {schlagworte.map((wort, index) => <Chip key={index} label={wort} size='small' />)}
+          {schlagworte?.map((wort, index) => <Chip key={index} label={wort} size='small' />)}
         </Stack>
       </CardContent>
     </Card>
